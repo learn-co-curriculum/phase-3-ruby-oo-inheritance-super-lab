3aa5cc2aec6d3472bd4329af9392f87db670efda
ihollander <ianholla@gmail.com>
2021-07-12 08:28:10 -0400

    Fixed formatting

--- spec/chatty_student_spec.rb
+++ spec/chatty_student_spec.rb
@@ -1,18 +1,17 @@
-require "spec_helper"
-
-describe "ChattyStudent" do 
-  
+describe ChattyStudent do
   let!(:chatty_student) { ChattyStudent.new }
   
   describe "#hello" do
-    it "returns a greeting phrase plus a long chatty phrase" do  
-      expect{chatty_student.hello}.to output("Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n").to_stdout
+    it "returns a greeting phrase plus a long chatty phrase" do
+      expected_output = "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+      expect { chatty_student.hello }.to output(expected_output).to_stdout
     end
   end
 
   describe "#raise_hand" do 
-    it "returns the phrase 'Pick me!' ten times." do 
-      expect{chatty_student.raise_hand}.to output("Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n").to_stdout
+    it "returns the phrase 'Pick me!' ten times." do
+      expected_output = "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
+      expect { chatty_student.raise_hand }.to output(expected_output).to_stdout
     end
   end
 end
